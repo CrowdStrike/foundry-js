ee1d0a92f7c80eda69dd78d2dc3ef27a71ac3547
Ruslan Zavacky <ruslan.zavacky@crowdstrike.com>
2023-05-04 18:38:57 +0300

    feat: introduce `isInitialized` concept and onInitialized callback
    This callback allows application to control their app state, and wait for communication layer to be established, before rendering an app.

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,25 @@ interface ReadyEventData {
 export const falcon = {
   bridge: getBridgeInstance(),
 
-  init() {
+  isInitialized: false,
+
+  init(onInitialized = () => {}) {
+    if (this.isInitialized) {
+      return;
+    }
+
     const handleReadyEvent = (data: ReadyEventData) => {
       if (data.payload.name === PLATFORM_EVENTS.READY) {
         this.bridge.message.off(handleReadyEvent);
 
         this.bridge.setOrigin(data.payload?.origin);
         this.bridge.postMessage({ type: PLATFORM_EVENTS.READY });
+
+        this.isInitialized = true;
+
+        if (typeof onInitialized === 'function') {
+          onInitialized();
+        }
       }
     };
 
