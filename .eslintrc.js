ffd84ce140d930e46973a7484e8c892c64a49e74
Simon Ihmig <simon.ihmig@gmail.com>
2023-05-08 18:07:20 +0200

    Setup ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,32 @@
-module.exports = {};
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {
+    '@typescript-eslint/no-empty-interface': 'warn',
+  },
+  overrides: [
+    {
+      files: ['./.eslintrc.js', './.prettierrc.js'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+    },
+  ],
+};
