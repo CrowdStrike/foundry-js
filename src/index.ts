0c8edb2b62ae350820ca592ad52cd79b28a8d24e
Simon Ihmig <simon.ihmig@gmail.com>
2023-05-12 20:04:09 +0200

    Reverse order of "ready" handshake
    Instead of making the main thread (falcon-console) send the first "ready" postmessage event (which assumes the UI extension is already listening for at this point, which might not be true always), we can reverse the order and let the UI extension make the first request (by calling await api.connect())

--- src/index.ts
+++ src/index.ts
@@ -2,43 +2,14 @@ import FalconPublicApis from './apis/public-api';
 
 import { PLATFORM_EVENTS } from './apis/types';
 
-interface ReadyEventData {
-  payload: {
-    name: 'Ready';
-    origin: string;
-  };
-}
-
-const CONNECTION_TIMEOUT = process.env.VITEST ? 1 : 5000;
-
 export default class FalconApi extends FalconPublicApis {
   async connect(): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      const timeoutTimer = setTimeout(() => {
-        this.bridge.message.off(handleReadyEvent);
-        reject(
-          new Error(
-            `Connection to foundry host timed out after ${CONNECTION_TIMEOUT}ms!`
-          )
-        );
-      }, CONNECTION_TIMEOUT);
-
-      const handleReadyEvent = (data: ReadyEventData) => {
-        if (data.payload.name === PLATFORM_EVENTS.READY) {
-          this.bridge.message.off(handleReadyEvent);
-          clearTimeout(timeoutTimer);
-
-          this.bridge.setOrigin(data.payload?.origin);
-          this.bridge.postMessage({ type: PLATFORM_EVENTS.READY });
-
-          this.isConnected = true;
-
-          resolve();
-        }
-      };
-
-      this.bridge.message.on(handleReadyEvent);
+    const data: any = await this.bridge.postMessage({
+      type: PLATFORM_EVENTS.READY,
     });
+
+    this.bridge.setOrigin(data.origin);
+    this.isConnected = true;
   }
 
   destroy() {
