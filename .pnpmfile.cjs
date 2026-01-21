0ab7d697fa1bda06a4785cedc24174b255b0af83
Ryan Hinchey <160072195+rhinchey-cs@users.noreply.github.com>
2026-01-21 09:50:11 -0500

    chore: update dependencies and add .pnpmfile to remove tarball urls (#227)

---
+++ .pnpmfile.cjs
@@ -0,0 +1,15 @@
+function afterAllResolved(lockfile) {
+  // Remove the tarball URLs from the lockfile
+  for (const key in lockfile.packages) {
+    if (lockfile.packages[key].resolution?.tarball) {
+      delete lockfile.packages[key].resolution.tarball;
+    }
+  }
+  return lockfile;
+}
+
+module.exports = {
+  hooks: {
+    afterAllResolved,
+  },
+};
